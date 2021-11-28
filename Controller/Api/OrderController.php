4dbce452d162927b097d381599ca4ddc1020d46b
Binh.NguyenDuc2000 <76272856+BinhNguyenDuc2000@users.noreply.github.com>
2021-11-28 20:54:52 +0700

    API update

--- Controller/Api/OrderController.php
+++ Controller/Api/OrderController.php
@@ -5,27 +5,42 @@ class OrderController extends BaseController
     {
         $strErrorDesc = '';
         $requestMethod = $_SERVER["REQUEST_METHOD"];
-        $arrQueryStringParams = $this->getQueryStringParams();
-        if (strtoupper($requestMethod) == 'GET') {
+        if (strtoupper($requestMethod) == 'POST') {
             try {
                 $orderModel = new OrderModel();
- 
+
                 $intLimit = 10;
-                if (isset($arrQueryStringParams['limit']) && $arrQueryStringParams['limit']) {
-                    $intLimit = $arrQueryStringParams['limit'];
+                if (isset($_POST['limit']) && $_POST['limit'] > 0) {
+                    $intLimit = $_POST['limit'];
                 }
-                
-                $arrOrders = $orderModel->getOrders($intLimit);
-                $responseData = json_encode($arrOrders);
+
+                if (isset($_POST['api_token_key'])) {
+                    $apiTokenKey = $_POST['api_token_key'];
+                } else {
+                    throw new InvalidArgumentException("Invalid Argument");
+                }
+
+                $arrOrders = $orderModel->getOrders($intLimit, $apiTokenKey);
+                if ($arrOrders != NULL) {
+                    $responseData = json_encode($arrOrders);
+                } else {
+                    throw new Exception("Can't find any order");
+                }
             } catch (Error $e) {
-                $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+                $strErrorDesc = $e->getMessage() . 'Something went wrong! Please contact support.';
                 $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+            } catch (InvalidArgumentException $e) {
+                $strErrorDesc = $e->getMessage();
+                $strErrorHeader = 'HTTP/1.1 400 Bad Request';
+            } catch (Exception $e) {
+                $strErrorDesc = $e->getMessage();
+                $strErrorHeader = 'HTTP/1.1 404 Not Found';
             }
         } else {
             $strErrorDesc = 'Method not supported';
             $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
         }
- 
+
         // send output
         if (!$strErrorDesc) {
             $this->sendOutput(
@@ -33,7 +48,8 @@ class OrderController extends BaseController
                 array('Content-Type: application/json', 'HTTP/1.1 200 OK')
             );
         } else {
-            $this->sendOutput(json_encode(array('error' => $strErrorDesc)), 
+            $this->sendOutput(
+                json_encode(array('error' => $strErrorDesc)),
                 array('Content-Type: application/json', $strErrorHeader)
             );
         }
@@ -42,28 +58,36 @@ class OrderController extends BaseController
     {
         $strErrorDesc = '';
         $requestMethod = $_SERVER["REQUEST_METHOD"];
-        $arrQueryStringParams = $this->getQueryStringParams();
-        if (strtoupper($requestMethod) == 'GET') {
+        if (strtoupper($requestMethod) == 'POST') {
             try {
                 $orderModel = new OrderModel();
-                if (isset($arrQueryStringParams['order_id']) && $arrQueryStringParams['order_id']) {
-                    $orderId = $arrQueryStringParams['order_id'];
+                if (isset($_POST['order_id']) && isset($_POST['api_token_key'])) {
+                    $orderId = $_POST['order_id'];
+                    $apiTokenKey = $_POST['api_token_key'];
+                } else {
+                    throw new InvalidArgumentException("Invalid Argument");
                 }
-                else{
-                    throw new Exception("No order id given");
+                $arrOrder = $orderModel->getOrder($orderId, $apiTokenKey);
+                if ($arrOrder != NULL) {
+                    $responseData = json_encode($arrOrder);
+                } else {
+                    throw new Exception("Can't find any order");
                 }
-                
-                $arrOrder = $orderModel->getOrder($orderId);
-                $responseData = json_encode($arrOrder);
             } catch (Error $e) {
-                $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+                $strErrorDesc = $e->getMessage() . '. Something went wrong! Please contact support.';
                 $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+            } catch (InvalidArgumentException $e) {
+                $strErrorDesc = $e->getMessage();
+                $strErrorHeader = 'HTTP/1.1 400 Bad Request';
+            } catch (Exception $e) {
+                $strErrorDesc = $e->getMessage();
+                $strErrorHeader = 'HTTP/1.1 404 Not Found';
             }
         } else {
             $strErrorDesc = 'Method not supported';
             $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
         }
- 
+
         // send output
         if (!$strErrorDesc) {
             $this->sendOutput(
@@ -71,10 +95,10 @@ class OrderController extends BaseController
                 array('Content-Type: application/json', 'HTTP/1.1 200 OK')
             );
         } else {
-            $this->sendOutput(json_encode(array('error' => $strErrorDesc)), 
+            $this->sendOutput(
+                json_encode(array('error' => $strErrorDesc)),
                 array('Content-Type: application/json', $strErrorHeader)
             );
         }
     }
 }
-?>
--- Model/OrderModel.php
+++ Model/OrderModel.php
@@ -3,14 +3,18 @@ require_once PROJECT_ROOT_PATH . "/Model/Database.php";
  
 class OrderModel extends Database
 {
-    public function getOrders($limit)
+    public function getOrders($limit, $api_token_key)
     {
-        return $this->select("SELECT * FROM OrderTable ORDER BY order_id ASC LIMIT ?", ["i", $limit]);
+        return $this->select("SELECT * FROM OrderTable
+                            WHERE user_id in (SELECT user_id FROM UserTable WHERE api_token_key = \"$api_token_key\")
+                            ORDER BY order_id ASC LIMIT ?", ["i", $limit]);
     }
 
-    public function getOrder($order_id)
+    public function getOrder($order_id, $api_token_key)
     {
-        return $this->select("SELECT * FROM OrderTable WHERE order_id = $order_id");
+
+        return $this->select("SELECT * FROM OrderTable WHERE order_id = $order_id 
+                            AND user_id in (SELECT user_id FROM UserTable WHERE api_token_key = \"$api_token_key\")");
     }
 }
 ?>
